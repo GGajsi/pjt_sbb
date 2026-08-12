d59d5fc26c31b352548d6b3ac0d53a039ec3411c
GGajsi <danghyeoncheong2@gmail.com>
2026-08-12 16:27:51 +0900

    Entity class 설계+h2 table 생성 /JPA 환경 설정

---
+++ src/main/java/com/example/sbb/Answer.java
@@ -0,0 +1,25 @@
+package com.example.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id  // answer entity --> pk id 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY)   // 신규 recode가 insert 될떄 자동으로 id의 값을 1씩 증가
+    private Integer id;
+
+    @Column(columnDefinition = "Text") // field 타입을 text로 지정
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
