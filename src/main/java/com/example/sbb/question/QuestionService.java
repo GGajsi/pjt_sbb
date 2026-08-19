2ae686cdf580ee6b5b8ed8f8f477c41c87b67ff0
GGajsi <danghyeoncheong2@gmail.com>
2026-08-19 12:53:07 +0900

    표준 HTML 구조로 변경하기

--- src/main/java/com/example/sbb/question/QuestionService.java
+++ src/main/java/com/example/sbb/question/QuestionService.java
@@ -15,7 +15,7 @@ public class QuestionService {
     private final QuestionRepository questionRepository;
 
     public List<Question> getList() {
-        return this.questionRepository.findAll();
+        return this.quwestionRepository.findAll();
     }
 
     public Question getQuestion(Integer id) {
